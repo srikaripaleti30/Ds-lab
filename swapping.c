67a72f2fe28650f4ea3613863788bdf744ab0657
srikaripaleti30 <srikaripaleti79@gmail.com>
2025-07-29 12:37:42 +0530

    Create swapping.c

---
+++ swapping.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a, b;
+clrscr(); // Note: This is Turbo C-specific. Not standard in GCC.
+
+printf("\nEnter the values of a and b: ");
+scanf("%d %d", &a, &b);
+
+printf("\nBefore swapping a = %d\t b = %d", a, b);
+
+// Logic begins
+a = a + b;
+b = a - b;
+a = a - b;
+
+printf("\nAfter swapping a = %d\t b = %d", a, b);
+return 0;
+}
